e6547d4e69a448b5e73c4bd91f3ced0fc59147c7
swati1408 <swatisaini1408@gmail.com>
2024-06-18 15:05:49 +0000

    comparion operators

--- .devcontainer/01_basics/03_conversionOrOperation.js
+++ .devcontainer/01_basics/03_conversionOrOperation.js
@@ -27,3 +27,35 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+3);
+//console.log(2-3);
+//console.log(2*3);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%4);
+
+let str1 = "hello"
+let str2 = " Swati"
+let str3 = str1+str2
+//console.log(str3);
+
+//console.log(1 + "2");
+//console.log("1" + 2);
+//console.log("1" + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 1 + "2");
+
+//console.log(+true);
+//console.log(+false);
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);
+
+
+
