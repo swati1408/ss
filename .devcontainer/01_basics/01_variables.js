8e8953bc4f45a36b690be635f49322cbbd5589a3
swati1408 <swatisaini1408@gmail.com>
2024-06-18 11:10:27 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = "144567"
+let accountEmail = "swati@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+//accountId = 2 //not allowed
+
+accountEmail = "hitesh@gmail.com"
+accountPassword = "21212"
+accountCity = "pinjore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
