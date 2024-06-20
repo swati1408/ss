1d419dc570e3c454ad80d29234e09fc2738fd86e
swati1408 <swatisaini1408@gmail.com>
2024-06-20 09:59:46 +0000

    math and  num

--- .devcontainer/01_basics/06_nums_and_maths.js
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -1,9 +1,37 @@
 const score = 400
-console.log(score);
+//console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(1));
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber = 122.1489
+
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//*****************************************MATHS********************************** 
+
+//console.log(Math);
+//console.log(Math.abs(-5));
+//console.log(Math.round(7.4));
+//console.log(Math.ceil(4.7));
+//console.log(Math.floor(4.9));
+//console.log(Math.max(2,3,6,7));
+//console.log(Math.min(1,56,7,));
+
+//console.log(Math.random());
+//console.log(Math.random()*10+1);
+
+console.log((Math.floor(Math.random()*10)+1));
+
+const min = 10
+const max = 20
+
+console.log(Math.random() * (max-min + 1));
+
 
