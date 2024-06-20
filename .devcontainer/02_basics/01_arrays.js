25959fb6b0846e603adcfc7503625b28c2b8f0c3
swati1408 <swatisaini1408@gmail.com>
2024-06-20 13:22:37 +0000

    array

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -14,7 +14,33 @@ myArr.shift()
 
 //console.log(myArr);
 
-console.log(myArr.includes(9));
-console.log(myArr);
+//console.log(myArr.includes(9));
+//console.log(myArr);
+
+//console.log(myArr.indexOf(3));
+
+//const newArr = myArr.join
+//console.log(myArr);
+
+//console.log(newArr);
+
+//slice, splice
+
+//console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+//console.log(myn1);
+//console.log("A", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
+
+
 
 
