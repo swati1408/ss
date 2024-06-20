9663a790615fe3895da2138ab756a37c525fcf10
swati1408 <swatisaini1408@gmail.com>
2024-06-20 11:05:06 +0000

    Array

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,20 @@
+//Array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+//console.log(myArr[0]);
+
+//ARRAY METHODES
+
+myArr.push(6)
+//console.log(myArr);
+
+myArr.unshift(9)
+myArr.shift()
+
+//console.log(myArr);
+
+console.log(myArr.includes(9));
+console.log(myArr);
+
+
