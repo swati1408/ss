1c9afb18c365fc816bce12746c4eb544e830f892
swati1408 <swatisaini1408@gmail.com>
2024-06-24 09:20:04 +0000

    array2

--- .devcontainer/02_basics/02_array.js
+++ .devcontainer/02_basics/02_array.js
@@ -7,3 +7,24 @@ marvel_heros.push(dc_heros)
 
  const allHeros = marvel_heros.concat(dc_heros)
 //console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+//console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4,5]]]
+ const real_another_array = another_array.flat(Infinity)
+ //console.log(real_another_array);
+
+ console.log(Array.isArray("Hitesh"))
+ console.log(Array.from("Hitesh"))
+ console.log(Array.from({name:"hitesh"}))   //INTERESTING
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
