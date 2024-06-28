32bb845290bd928d04bacb4eb454e8de615f43b6
swati1408 <swatisaini1408@gmail.com>
2024-06-28 15:45:01 +0000

    object in detail

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,62 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Swati"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email: "swatigoogle.com",
+    fullname: {
+        userfullname:{
+            firstname: "swati",
+            lastname: "saini"
+        }
+    }
+}
+
+//console.log(regularUser.fullname?.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({},obj1, obj2)
+//console.log(obj3);
+
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+const users = [
+{
+    id: 1,
+    email: "ssgmail.com"
+},
+{
+},
+
+]
+
+users[1].email
+//console.log(tinderUser);
+
+//console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
+
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+const course = {
+    coursename: "javascript",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+const {courseInstructor: instructor} = course       //destructuring of object
+
+//console.log(instructor);
+
+
+
+
