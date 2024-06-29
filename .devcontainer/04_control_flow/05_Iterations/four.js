f094c7c6ee95b7ba311abe68de1c86396dc1ddcb
swati1408 <swatisaini1408@gmail.com>
2024-06-29 11:58:52 +0000

    map in object

---
+++ .devcontainer/04_control_flow/05_Iterations/four.js
@@ -0,0 +1,17 @@
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for(const key in myObj) {
+   // console.log(myObj[key]);
+    
+   console.log(`${key} shortcut is for ${myObj[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for(const key in programming) {
+    console.log(programming[key]);
+}
