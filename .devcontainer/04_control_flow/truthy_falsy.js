ee497019e33329500fc1e3b5e6c51bb2c3aee9af
swati1408 <swatisaini1408@gmail.com>
2024-06-29 09:28:40 +0000

    truthy falsy values

---
+++ .devcontainer/04_control_flow/truthy_falsy.js
@@ -0,0 +1,40 @@
+const userEmail = "ss@gmail.com"
+
+//if(userEmail){
+  //  console.log("Got user Email");
+//} else {
+  //  console.log("Don't have user email");
+//}
+
+// falsy values
+//false, 0, -0, BigInt 0n, "", null, undefine, NaN
+
+//truthy values
+// 'false', "0", " ", {}, [], function(){}
+
+
+//if(userEmail.length === 0)
+  //  {
+    //    console.log("Array is empty");
+    //}
+
+
+  //******************** */ NULLISH COALESCING OPERATOR (??) : null undefined*******************
+
+  let val1;
+  //val1 = 5??10
+// val1 = null??10
+//val1 = undefined??10 
+  //console.log(val1);
+
+
+
+  //Ternary operator
+
+  //condition ? true : false
+
+  const iceTeaPrice = 100
+  iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+
+
+
