a990db012f24c4cbd8e3bbd17301f92513895350
swati1408 <swatisaini1408@gmail.com>
2024-06-29 11:10:29 +0000

    map

---
+++ .devcontainer/04_control_flow/05_Iterations/three.js
@@ -0,0 +1,25 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5]
+
+for(const num of arr) {
+    //console.log(num);
+}
+ 
+const greetings = "Hello world!"
+for(const greet of greetings) {
+   // console.log(`Each char is ${greet}`);
+}
+
+//Maps
+
+const map = new Map()
+map.set('IN', "INDIA");
+map.set('USA', "UNITED STATES OF AMERICA" );
+map.set('Fr', "France")
+
+console.log(map);
+
+for(const [key,value] of map) {
+    console.log(key , ':-', value);
+}
